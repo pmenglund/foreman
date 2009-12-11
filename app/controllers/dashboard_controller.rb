a7ff63ada081dbbb0b9e30718351e9f6d5f09b8e
Ohad Levy <ohadlevy@gmail.com>
2009-12-12 00:04:43 +0800

    fixing wrong controller name and fixing logical operations

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,8 +4,8 @@ class DashboardController < ApplicationController
 
   def index
     @total_hosts = Host.count
-    @bad_hosts = Host.with("failed").with("failed_restarts").with("skipped").count
-    @active_hosts = Host.with("applied").with("restarted").count
+    @bad_hosts = Host.with("failed").count + Host.with("failed_restarts").count + Host.with("skipped").count
+    @active_hosts = Host.with("applied").count +  Host.with("restarted").count
     @good_hosts = Host.count(:all, :conditions => @good_reports) - @bad_hosts
     @out_of_sync_hosts = Host.out_of_sync.count
     @intersting_reports = Report.count(:all, :conditions => @report_conditions)
@@ -15,13 +15,13 @@ class DashboardController < ApplicationController
 
   def errors
     render :partial => "hosts/minilist", :layout => true, :locals => {
-      :hosts => Host.with("failed").with("failed_restarts").with("skipped"),
+      :hosts => (Host.with("failed") + Host.with("failed_restarts") + Host.with("skipped")),
       :header => "Hosts with errors" }
   end
 
   def active
     render :partial => "hosts/minilist", :layout => true, :locals => {
-      :hosts => Host.with("applied").with("restarted"),
+      :hosts => (Host.with("applied") + Host.with("restarted")),
       :header => "Active Hosts" }
   end
 
